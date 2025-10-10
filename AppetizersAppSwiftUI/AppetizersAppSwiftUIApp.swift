bebb1fe5991bd359cbb76222bb428ae590d54636
Emirhan İpek <emirrhanipek@gmail.com>
2025-10-10 18:09:40 +0300

    Initial Commit

---
+++ AppetizersAppSwiftUI/AppetizersAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersAppSwiftUIApp.swift
+//  AppetizersAppSwiftUI
+//
+//  Created by Emirhan Ipek on 10.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
